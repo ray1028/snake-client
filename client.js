09fc7d13305810e0c833f4d38f314ad10a66654e
rayjay1028 <rayjay1028@gmail.com>
2019-07-31 17:48:53 +0000

    modified client.js by putting all conn statement within the connect

--- client.js
+++ client.js
@@ -11,18 +11,17 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
-  conn.on("data", data => {
-    console.log(data);
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+
+    conn.write("Name: RAY");
+
+    conn.on("data", data => {
+      console.log(data);
+    });
   });
-
-  conn.on('connect', () => {
-    console.log('Successfully connected to game server');
-    conn.write('Name: RAY');
-  })
-
 
   return conn;
 };
 
 module.exports = { connect };
-
